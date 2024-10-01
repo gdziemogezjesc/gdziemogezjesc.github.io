9cd38c7ff4956f7fefcc7cb3a7ea71d55d4b4cde
gdziemogezjesc <qwertury123@gmail.com>
2024-10-01 14:47:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 document.getElementById('search-button').addEventListener('click', searchText);
 
 function searchText() {
+
     const searchQuery = document.getElementById('search-input').value.toLowerCase();
-    
+
     const containers = document.querySelectorAll('.container');
-    
+
     containers.forEach(container => {
         container.classList.remove('hidden');
     });
     
     if (searchQuery.trim() !== "") {
-        const searchTags = searchQuery.split(',').map(tag => tag.trim()); 
+
+        const searchTags = searchQuery.split(',').map(tag => tag.trim());
 
         containers.forEach(container => {
             const textContent = container.querySelector('.text-content p').textContent.toLowerCase();
             
+
             const allTagsPresent = searchTags.every(tag => textContent.includes(tag));
             
+
             if (!allTagsPresent) {
                 container.classList.add('hidden');
             }
